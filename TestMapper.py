defb743dc41d324970d74abe1496233b9220326a
cjj4 <30358844+cjj4@users.noreply.github.com>
2019-02-12 20:17:34 -0800

    Update TestMapper.py

--- TestMapper.py
+++ TestMapper.py
@@ -5,8 +5,11 @@ import sys
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
+    
     # remove leading and trailing whitespace
+    
     line = line.strip()
+    
     # split the line into words
     words = line.split()
     # increase counters
